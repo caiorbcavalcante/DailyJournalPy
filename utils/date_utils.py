bf5a58b850c574fda7f54f880e0f1530614af39e
CaioVictor <caiorbcavalcante@gmail.com>
2025-08-22 12:10:35 -0300

    Melhoria da função getMonth

--- utils/date_utils.py
+++ utils/date_utils.py
@@ -10,32 +10,11 @@ def checkDate(data_str, formato="%d/%m/%Y"):
 date = checkDate("25/04/2006")
 
 def getMonth(dateMonth):
-    match dateMonth:
-        case 1:
-            return "Janeiro"
-        case 2:
-            return "Fevereiro"
-        case 3:
-            return "Março"
-        case 4:
-            return "Abril"
-        case 5:
-            return "Maio"
-        case 6:
-            return "Junho"
-        case 7:
-            return "Julho"
-        case 8:
-            return "Agosto"
-        case 9:
-            return "Setembro"
-        case 10:
-            return "Outubro"
-        case 11:
-            return "Novembro"
-        case 12:
-            return "Dezembro"
-        case _:
-            return "Mês inválido"
+    meses = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho",
+             "Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"]
+    if 1 <= dateMonth <= 12:
+        return meses[dateMonth-1]
+    return "Mês inválido"
+
 
 
